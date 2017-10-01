4ebfb83d9f12f7adc38304d6eb47a4853b95f995
Drot88 <Daniel.Rotenberg88@gmail.com>
2017-10-01 14:59:54 -0400

    implemented bubble sorting algorithm for Q3

--- Q3.c
+++ Q3.c
@@ -5,26 +5,54 @@
  */
  
 #include <stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 
 void sort(int* number, int n){
      /*Sort the given array number , of length n*/     
+  int i,j;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n-1; j++) {
+      if (number[j] > number[j+1]) {
+        int temp = number[j];
+        number[j] = number[j+1];
+        number[j+1] = temp;
+      }
+    }
+  }
+
 }
 
 int main(){
+    srand(time(0));
     /*Declare an integer n and assign it a value of 20.*/
     int n = 20;
     /*Allocate memory for an array of n integers using malloc.*/
-    
+    int *array = malloc(n * sizeof(int));
     /*Fill this array with random numbers, using rand().*/
-    
+    int x;
+
+    for (x = 0; x < n; x++) {
+      int randNum = rand() % 1000 + 1;
+      array[x] = randNum;
+    }
     /*Print the contents of the array.*/
 
+    for (x = 0; x < n; x++) {
+      printf("Array[%d]: %d\n", x, array[x]);
+    }
+
     /*Pass this array along with n to the sort() function of part a.*/
-    
+    sort(array, n);
+    printf("\nArray has been sorted\n");
     /*Print the contents of the array.*/    
     
+    for (x = 0; x < n; x++) {
+      printf("Array[%d]: %d\n", x, array[x]);
+    }
+
     return 0;
 }
